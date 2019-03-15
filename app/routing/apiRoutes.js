46b9e2dea61636b7cd1492121f5370afed6dfeff
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-15 07:12:04 -0500

    Modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,10 @@ module.exports = function (app) {
     })
     app.post('/api/friends', function (req, res) {
         var newUser = req.body;
+        // Create variable as a placeholder - updates with scores that are the closest
         var bestMatch = {
             name: "",
+            photo: "",
             scoreDifference: 1000
         }
         for (i = 0; i < friendData.length; i++) {
@@ -25,10 +27,14 @@ module.exports = function (app) {
             }
             if (totalDifference < bestMatch.scoreDifference) {
                 bestMatch.name = friendCompare.friendName
+                bestMatch.photo = friendCompare.friendPhoto
                 bestMatch.scoreDifference = totalDifference
             }
         }
         console.log("Best Match: " + bestMatch.name);
         friendData.push(req.body);
-    })
+        console.log(friendData);
+        console.log(bestMatch);
+        res.json(bestMatch);
+    });
 }
