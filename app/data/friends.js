2d4c641ac85729f6dc745a84d16e1b0c00979add
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-15 07:58:53 -0500

    Added Friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,12 +4,12 @@ var friendArray = [{
         friendScore: [
             "5",
             "1",
-            "3",
+            "5",
             "1",
             "3",
             "4",
             "1",
-            "3",
+            "1",
             "5",
             "3"
         ]
@@ -19,9 +19,9 @@ var friendArray = [{
         friendPhoto: 'https://images1.westword.com/imager/u/745xauto/11262056/jan_michael_vincent_airwolf.jpg',
         friendScore: [
             "5",
-            "2",
             "3",
-            "1",
+            "3",
+            "2",
             "3",
             "4",
             "5",
@@ -40,11 +40,76 @@ var friendArray = [{
             "1",
             "3",
             "4",
+            "4",
+            "2",
+            "5",
+            "2"
+        ]
+    },
+    {
+        friendName: 'Bob Ross',
+        friendPhoto: 'https://yt3.ggpht.com/a-/AAuE7mC75lyDOGkH_0L4_ZR6F4bfGviFjsMZIGa22w=s900-mo-c-c0xffffffff-rj-k-no',
+        friendScore: [
+            "5",
+            "2",
+            "2",
+            "1",
+            "3",
+            "4",
             "5",
             "2",
             "5",
             "2"
         ]
+    },
+    {
+        friendName: 'Mr Rogers',
+        friendPhoto: 'https://timedotcom.files.wordpress.com/2018/06/mr-rogers.jpeg',
+        friendScore: [
+            "1",
+            "2",
+            "3",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "5",
+            "2"
+        ]
+    },
+    {
+        friendName: 'The Ultimate Warrior',
+        friendPhoto: 'https://cdn-images-1.medium.com/max/1200/1*T6CYFDGIDeBOecfdVn0YTw.jpeg',
+        friendScore: [
+            "5",
+            "2",
+            "3",
+            "1",
+            "2",
+            "4",
+            "5",
+            "2",
+            "5",
+            "2"
+        ]
+    },
+    {
+        friendName: 'Mrs. Garrett',
+        friendPhoto: 'https://ewedit.files.wordpress.com/2017/04/gettyimages-138462639.jpg',
+        friendScore: [
+            "5",
+            "2",
+            "3",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "4",
+            "2"
+        ]
     }
+
 ]
 module.exports = friendArray;
